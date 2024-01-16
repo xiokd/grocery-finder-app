795d454c6ff7919fea5741cdfb8391eac81feaf5
Keng Xiong <74529239+xiokd@users.noreply.github.com>
2024-01-16 10:31:49 -0800

    Completed UI for Store Map page

---
+++ grocery-app/src/components/StoreCard.jsx
@@ -0,0 +1,34 @@
+import { Box, Card, CardContent, Typography, CardMedia } from "@mui/material";
+
+import placeholder from "../images/placeholder/placeholderstore.png";
+
+function StoreCard() {
+  return (
+    <div class="store-card">
+      <Card sx={{ display: "flex" }}>
+        <CardMedia
+          component="img"
+          sx={{ width: 151 }}
+          image={placeholder}
+          alt="Placeholder Store"
+        />
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <CardContent sx={{ flex: "1 0 auto" }}>
+            <Typography component="div" variant="h6">
+              Placeholder Store
+            </Typography>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              component="div"
+            >
+              Brief description about the store
+            </Typography>
+          </CardContent>
+        </Box>
+      </Card>
+    </div>
+  );
+}
+
+export default StoreCard;
--- grocery-app/src/pages/StoreMap.jsx
+++ grocery-app/src/pages/StoreMap.jsx
@@ -1,14 +1,34 @@
 import React from "react";
 import "../index.css";
-
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
 import NavBar from "../components/NavBar";
+import StoreCard from "../components/StoreCard";
 
 function StoreMap() {
+  // Default Coordinates set to Cheney WA
+  const position = { lat: 47.487389, lng: -117.575762 };
+
+  // Change VITE_GOOGLE_MAPS_API_KEY value in the .env file to API Key value
   return (
     <div>
-      <NavBar/>
-      <div>
-        StoreMap Page
+      <NavBar />
+      <div class="map-page-container">
+        <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+          <div class="map-container">
+            <Map zoom={15} center={position}></Map>
+          </div>
+        </APIProvider>
+        <div class="store-container no-scrollbar">
+          <div class="store-card-container">
+            <StoreCard />
+            <StoreCard />
+            <StoreCard />
+            <StoreCard />
+            <StoreCard />
+            <StoreCard />
+            <StoreCard />
+          </div>
+        </div>
       </div>
     </div>
   );
