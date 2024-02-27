98dc110afc531eb2df6cbe975503838389457637
Camquistador <colivier@ewu.edu>
2024-02-26 21:44:34 -0800

    Map working

--- grocery-app/src/pages/StoreMap.jsx
+++ grocery-app/src/pages/StoreMap.jsx
@@ -1,43 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "../index.css";
 import { APIProvider, Map } from "@vis.gl/react-google-maps";
+
 import NavBar from "../components/NavBar";
 import StoreCard from "../components/StoreCard";
-import ParentComponent from './ParentComponent'; // Import ParentComponent
 
-function StoreMap({ location, distance }) { // Receive location and distance as props
+function StoreMap({ location, distance, onSearch }) {
     const position = { lat: 47.487389, lng: -117.575762 };
-    const [mapView, setMapView] = useState({ center: position, zoom: 15 }); // Initial map view
+    const [mapView, setMapView] = useState({ center: position, zoom: 15 });
 
     useEffect(() => {
-        // Function to update map view based on location and distance
         const updateMapView = () => {
-            // Check if location is valid
-            if (location.lat !== null && location.lng !== null) {
-                // Set the center of the map to the specified location
+            if (location && location.lat !== null && location.lng !== null) {
                 const newCenter = { lat: location.lat, lng: location.lng };
-                // Set the zoom level based on the distance value
-                let newZoom = 15; // Default zoom level
+                let newZoom = 15;
                 if (distance === 2) {
                     newZoom = 12;
                 } else if (distance === 3) {
                     newZoom = 10;
                 } else if (distance === 5) {
                     newZoom = 8;
-                } // Adjust zoom level based on other distance values as needed
-                // Update map view with new center and zoom level
+                }
                 setMapView({ center: newCenter, zoom: newZoom });
             } else {
                 console.error("Invalid location");
             }
         };
 
-        updateMapView(); // Call the function initially and whenever location or distance changes
+        updateMapView();
     }, [location, distance]);
 
-    // Handle click event of search icon
     const handleSearch = () => {
-        // Call updateMapView function when the user clicks the search icon
-        updateMapView();
+        onSearch(location, distance);
     };
 
     return (
@@ -61,7 +55,6 @@ function StoreMap({ location, distance }) { // Receive location and distance as 
                     </div>
                 </div>
             </div>
-            <ParentComponent /> {/* Render ParentComponent */}
         </div>
     );
 }
