b85f269662a8220984e450faba49c3fdf648951a
Matthew-Powers <mpowers16@ewu.edu>
2024-03-17 00:04:38 -0700

    Very small change that probably doesn't fix anything

--- grocery-app/src/context/StoreContext.jsx
+++ grocery-app/src/context/StoreContext.jsx
@@ -3,7 +3,7 @@ import React, {useState, createContext} from "react";
 export const StoreContext = createContext();
 
 //wrapper for application so there is access to the state
-export const StoreContextProvider = props => {
+export const StoreContextProvider = (props) => {
     const [store, setStore] = useState([]);
 
 
